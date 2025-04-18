8f82e430bb0e32062d1f7e76dc2d4a9be121ef19
liuwenzheng <329541594@qq.com>
2025-04-18 15:14:33 +0800

    提取通用ui资源，改为引用库

--- app/src/main/java/com/moko/tofsensortest/activity/BaseActivity.java
+++ app/src/main/java/com/moko/tofsensortest/activity/BaseActivity.java
@@ -8,8 +8,8 @@ import android.os.Bundle;
 import android.os.SystemClock;
 
 import com.elvishew.xlog.XLog;
-import com.moko.tofsensortest.view.LoadingDialog;
-import com.moko.tofsensortest.view.LoadingMessageDialog;
+import com.moko.lib.bxpui.dialog.LoadingDialog;
+import com.moko.lib.bxpui.dialog.LoadingMessageDialog;
 
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
--- app/src/main/java/com/moko/tofsensortest/activity/GuideActivity.java
+++ app/src/main/java/com/moko/tofsensortest/activity/GuideActivity.java
@@ -11,9 +11,10 @@ import android.os.Handler;
 import android.os.Looper;
 import android.provider.Settings;
 
+import com.elvishew.xlog.XLog;
+import com.moko.lib.bxpui.dialog.PermissionDialog;
 import com.moko.tofsensortest.R;
 import com.moko.tofsensortest.utils.Utils;
-import com.moko.tofsensortest.view.PermissionDialog;
 import com.permissionx.guolindev.PermissionX;
 
 import androidx.activity.result.ActivityResultLauncher;
@@ -21,6 +22,9 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.core.content.ContextCompat;
 
 public class GuideActivity extends BaseActivity {
+
+    private String mAppName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +33,7 @@ public class GuideActivity extends BaseActivity {
             finish();
             return;
         }
+        mAppName = getString(R.string.app_name);
         requestPermission();
     }
 
@@ -40,8 +45,8 @@ public class GuideActivity extends BaseActivity {
                 return;
             }
             if (!isWriteStoragePermissionOpen() || !isLocationPermissionOpen()) {
-                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION}, getResources().getString(R.string.permission_storage_need_content),
-                        getResources().getString(R.string.permission_storage_close_content));
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION}, getResources().getString(R.string.permission_storage_need_content, mAppName, mAppName),
+                        getResources().getString(R.string.permission_storage_close_content, mAppName));
                 return;
             }
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
@@ -52,8 +57,8 @@ public class GuideActivity extends BaseActivity {
             }
             //申请定位权限 BLUETOOTH BLUETOOTH_ADMIN不属于动态权限
             if (!isLocationPermissionOpen()) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, getResources().getString(R.string.permission_location_need_content),
-                        getResources().getString(R.string.permission_location_close_content));
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, getResources().getString(R.string.permission_location_need_content, mAppName, mAppName),
+                        getResources().getString(R.string.permission_location_close_content, mAppName));
                 return;
             }
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
@@ -64,8 +69,8 @@ public class GuideActivity extends BaseActivity {
             }
             if (!hasBlePermission() || !isLocationPermissionOpen()) {
                 requestPermissions(new String[]{Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_SCAN,
-                                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, getResources().getString(R.string.permission_ble_content),
-                        getResources().getString(R.string.permission_ble_close_content));
+                                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, getResources().getString(R.string.permission_ble_content, mAppName, mAppName),
+                        getResources().getString(R.string.permission_ble_close_content, mAppName));
                 return;
             }
         }
@@ -81,9 +86,10 @@ public class GuideActivity extends BaseActivity {
 
     private void requestPermissions(String[] permissions, String requestContent, String closeContent) {
         PermissionX.init(this).permissions(permissions).
-                onExplainRequestReason((scope, deniedList) -> scope.showRequestReasonDialog(new PermissionDialog(deniedList, requestContent))).
+                onExplainRequestReason((scope, deniedList) -> scope.showRequestReasonDialog(new com.moko.lib.bxpui.dialog.PermissionDialog(deniedList, requestContent))).
                 onForwardToSettings((scope, deniedList) -> scope.showForwardToSettingsDialog(new PermissionDialog(deniedList, closeContent))).
                 request((allGranted, grantedList, deniedList) -> {
+                    XLog.i("333333" + allGranted);
                     if (allGranted) requestPermission();
                     else finish();
                 });
@@ -99,8 +105,8 @@ public class GuideActivity extends BaseActivity {
         AlertDialog dialog = new AlertDialog.Builder(this)
                 .setCancelable(false)
                 .setTitle(R.string.location_need_title)
-                .setMessage(R.string.location_need_content)
-                .setPositiveButton(getString(R.string.permission_open), (dialog1, which) -> {
+                .setMessage(getString(R.string.location_need_content, mAppName))
+                .setPositiveButton(getString(R.string.ok), (dialog1, which) -> {
                     Intent intent = new Intent();
                     intent.setAction(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                     startLauncher.launch(intent);
